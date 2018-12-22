312f40d2ed1d3506fefaf5d32502ff40efe053a1
Nitesh Parmar <niteshp2007@gmail.com>
2018-12-22 13:16:32 +0000

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -9,18 +9,27 @@ import random
 
 
 def start_game():
+    highScore = 100
     attempts = 0
     randNumber = random.randint(1,10)
     name = input("What is your name? ").capitalize()
     print("Hello {}, and welcome to the number guessing game".format(name))
+   
     print("I am thinking of a number between 1 and 10, what is it?")
     
     while True:
         attempts += 1
         guess = int(input("What is my number? "))
         if guess == randNumber:
-            print("You guessed correctly, it took {} attempt/s, thanks playing!!! \nThe Game will end".format(attempts))
-            break
+            print("You guessed correctly, it took {} attempt/s, thanks playing!!! \nThe Game will end".format(attempts))    
+            if attempts < highScore:
+                highScore = attempts    
+            if input("Would you like to play again? y/n? ").upper() == "Y":
+                 print("The current highscore/least number of attempts is {}".format(highScore))
+                 randNumber = random.randint(1,10)
+                 continue
+            else:
+                break
         elif guess < randNumber:
             print("The number is higher, try again")
         elif guess > randNumber:
